41306b2b684017a51a8ef85c0ecc5df757180b27
benkindt <benjamin.kindt@s2011.tu-chemnitz.de>
2017-12-19 11:20:52 +0100

    working time series panel

--- dist/datasource.js
+++ dist/datasource.js
@@ -50,9 +50,14 @@ System.register(["lodash"], function (_export, _context) {
 
         _createClass(GenericDatasource, [{
           key: "buildDrillRequest",
-          value: function buildDrillRequest(sql) {
+          value: function buildDrillRequest(sql, from, to, limit, interval) {
         	  console.log("---log buildDrillRequest---");
-        	  console.log(sql);
+        	  if(sql === 'undefined'){
+        		  return { 
+                  	"queryType" : "SQL", 
+                  	"query" : "SELECT salary as `value`, hire_date as `timestamp` FROM cp.`employee.json` WHERE hire_date > '1996-01-01' AND supervisor_id = 5"
+                  };
+        	  }
         	  console.log(this.url);
             return { 
             	"queryType" : "SQL", 
@@ -62,68 +67,41 @@ System.register(["lodash"], function (_export, _context) {
         }, {
           key: "query",
           value: function query(options) {
-        	  console.log("---log query---");
+        	  console.log("---log query ... test---");
         	  console.log(options);
             var query = this.buildQueryParameters(options);
             
+            console.log(query.targets);
+            
             query.targets = query.targets.filter(function (t) {
+            	console.log("query targets filter function");
+            	var pass;
+            	if((typeof t.hide) === 'undefined'){
+            		t.hide = false;
+            	}
                 return !t.hide;
             });
             
             if (query.targets.length <= 0) {
-              return this.q.when({ data: [] });
+            	console.log("query.targets.length <= 0 ... is " + query.targets.length);
+            	console.log(query.targets);
+            	return this.q.when({ data: [] });
             }
-            
-            var items = [];
-            var properties = [];
-
-            _.forEach(query.targets, function (target) {
-              if (target.item && !_.includes(items, target.item)) {
-                items.push(target.item);
-              }
-              if (target.property && !_.includes(properties, target.property)) {
-                properties.push(target.property);
-              }
-            });
-            if (items.length <= 0) {
-              return this.q.when({ data: [] });
-            }
-            var itemVal = items.join(' ');
-            // if not set, keep undefined to load all properties
-            var propertyVal = undefined;
-            if (properties.length > 0) {
-              propertyVal = properties.join(' ');
-            }
-            
-//            var temp = this.backendSrv.datasourceRequest({
-//                url: this.url + "/query.json",
-//                data: this.buildDrillRequest(query.targets[0].target),
-//                method: 'POST'
-//              });
-//            console.log(temp);
-//            return temp;
-            return this.loadData(itemVal, propertyVal, options.range.from.valueOf(), options.range.to.valueOf())
+            var timeFunction = function toTimestamp(strDate){
+     		   var stamp = Date.parse(strDate);
+    		   return stamp;
+    		}
+            console.log("b4 return loadData");
+            return this.loadData(query.targets[0].target, options.range.from.valueOf(), options.range.to.valueOf())
             .then(function (results) {
-              var data = results.map(function (v) {
-                var targetName = self.prefixName(self.url, "lf:", v.item, v.property);
-                var values = v.values;
-                // get the config entry to access scale etc.
-                var target = _.filter(query.targets, function (target) {
-                  var _item = target.item.split(/[ ]+/)[1];
-                  var _property = target.property.split(/[ ]+/)[1];
-                  return v.item === _item && v.property === _property;
-                })[0];
-                var scale = target && target.scale ? target.scale : 1;
-
-                if (typeof scale === 'undefined') {
-                  scale = 1;
-                }
-
-                var datapoints = values.map(function (d) {
-                  return [d.value * scale, d.time];
-                });
-                return { target: targetName, datapoints: datapoints };
+            	console.log("---loadDate .then---");
+              var datapoints = results.data.rows.map(function (v) {
+            	  var val = Number.parseInt(v.value);
+            	  var timest = timeFunction(v.timestamp);
+            	  return [val, timest];
               });
+              var data = [  { target: query.targets[0].target, datapoints: datapoints } ]; // maybe allow setting target name by input field?
+              console.log(data);
               return { data: data };
             });
           }
@@ -192,60 +170,32 @@ System.register(["lodash"], function (_export, _context) {
           }
         }, {
             key: 'loadData',
-            value: function loadData(items, properties, from, to) {
+            value: function loadData(sql, from, to) {
+            	console.log("---loadData--- items & properties following");
               var interval = Math.round((to - from) / this.maxDataPoints);
               var limit = this.maxDataPoints / this.buckets;
               var self = this;
               return this.backendSrv.datasourceRequest({
                 url: this.url + "/query.json",
-                data: this.buildDrillRequest(query.targets[0].target),
+                data: this.buildDrillRequest(sql, from, to, limit, interval),
                 method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                params: {
-                  items: items,
-                  properties: properties,
-                  from: from,
-                  to: to,
-                  limit: limit,
-                  interval: interval,
-                  op: 'avg'
-                }
+                headers: { 'Content-Type': 'application/json' }
+//                params: {
+//                  items: items,
+//                  properties: properties,
+//                  from: from,
+//                  to: to,
+//                  limit: limit,
+//                  interval: interval,
+//                  op: 'avg'
+//                }
               }).then(function (response) {
                 if (response.status === 200) {
-                  var loadOlderData = false;
-                  var promises = [];
                   if (!response.data) {
                     console.log("Oops! No response data?!");
                     console.log(response);
                   }
-                  Object.keys(response.data).forEach(function (item) {
-                    var newItemData = response.data[item];
-                    Object.keys(newItemData).forEach(function (property) {
-                      var newPropertyData = newItemData[property];
-                      if (!newPropertyData || newPropertyData.length == 0) {
-                        return;
-                      } else {
-                        // LF returns data in latest-first order
-                        newPropertyData.reverse();
-                      }
-
-                      if (newPropertyData.length == limit) {
-                        // limit reached, fetch earlier blocks, keep from
-                        // but stop at earliest time already read - 1
-                        var localTo = newPropertyData[0].time - 1;
-                        promises.push(self.loadData(item, property, from, localTo)
-                        .then(function (d) {
-                          // FIXME: d is an array! handle appropriately
-                          return { item: item, property: property, values: d[0].values.concat(newPropertyData) };
-                        }));
-                      } else {
-                        promises.push({ item: item, property: property, values: newPropertyData });
-                      }
-                    });
-                  });
-                  // see below
-                  //return Promise.all(promises);
-                  return self.promiseAll(promises);
+                  return Promise.resolve( response );
                 }
               });
             }
